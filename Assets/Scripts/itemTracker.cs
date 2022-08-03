90bcc508957de6a2d8ead186ba076c6c98d351a2
Deton8t <hardeeharris@gmail.com>
2022-08-03 19:23:35 -0400

    yeah

---
+++ Assets/Scripts/itemTracker.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemTracker : MonoBehaviour
+{
+    public static bool hasItem1 = false;
+    public static bool hasItem2 = false;
+    public static bool hasItem3 = false;
+    public static bool hasItem4 = false;
+    public static int fromRoom = 1;
+    public static float timeLeft = 600F;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
